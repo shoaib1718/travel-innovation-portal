5435a3d345c1a5cb82685ba0c2147cd468c6ec5b
Shoaib Janvekar <shoaibrjanvekar@gmail.com>
2026-06-27 23:24:55 +0530

    Added Entity Framework setup and Idea model

---
+++ TravelInnovationPortal/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TravelInnovationPortal.Models;
+
+namespace TravelInnovationPortal.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(
+            DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Idea> Ideas { get; set; }
+    }
+}
---
+++ TravelInnovationPortal/Models/Idea.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TravelInnovationPortal.Models
+{
+    public class Idea
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string EmployeeName { get; set; }
+
+        [Required]
+        public string EmployeeEmail { get; set; }
+
+        [Required]
+        public string ServiceName { get; set; }
+
+        [Required]
+        public string IdeaTitle { get; set; }
+
+        [Required]
+        public string IdeaDescription { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+    }
+}
